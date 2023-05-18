e147f0fcf865580494803937c41011d183dd6033
Raqsana <raqsana.alizade@wetravel.com>
2023-05-18 20:55:35 +0400

    update task1

--- classworks/cw_task1.dart
+++ classworks/cw_task1.dart
@@ -2,12 +2,17 @@ void main(List<String> args) {
   Map<String, List<int>> costumers = {
     'John Snow': [500, 200],
     'Elliot Anderson': [150, 900],
+     'Elliot Ander': [900],
   };
 
   costumers.forEach((key, value) {
-    for(int i=0; i< value.length;i++){
-      if(value[i] > 200) value[i]+=10;
+    if(value.length >= 2){
+      for(int i=0; i< value.length;i++){
+        if(value[i] > 200) value[i]+=10;
+      }
+      print("$key: $value");
+    }else{
+      print("The costumer $key has less than 2 cards and cards values: $value");
     }
-    print("$key: $value");
   });
 }
